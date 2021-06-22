5cd09412887832fbea34da7daaea4595ec6c0c00
Alan Vasconcelos <alandbh@gmail.com>
2021-06-22 20:54:37 -0300

    Autenticacao no login

--- js/autentica.js
+++ js/autentica.js
@@ -1,1 +1,38 @@
 var listaUsuarios = JSON.parse(window.localStorage.usuarios);
+window.usuarioLogado = null;
+
+window.addEventListener("load", function () {
+
+    let formulario = document.querySelector("#login");
+
+    let campoLogin = document.querySelector("#nomelog");
+    let campoSenha = document.querySelector("#senhalog");
+
+    
+    formulario.onsubmit = (evento)=> {
+        evento.preventDefault()
+        let senha = campoSenha.value;
+        let login = campoLogin.value;
+        console.log('submeter')
+
+        let erroTemplate = document.createElement('div');
+        erroTemplate.classList.add('w-100')
+
+        let usuarioRegistrado = listaUsuarios.find((item)=>item.email === login) || null;
+
+        if (usuarioRegistrado && usuarioRegistrado.senha === senha) {
+            console.log('ok');
+            usuarioLogado = usuarioRegistrado;
+            window.open("/mapa.html","_self")
+        } else {
+            erroTemplate.innerHTML = '<small class="text-danger">Senha incorreta<small>';
+            campoSenha.parentNode.appendChild(erroTemplate);
+        }
+
+        
+
+
+
+    }
+
+})
